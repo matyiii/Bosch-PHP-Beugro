e07c8c363b75c62620e29d0c4c9949eee363e4ef
matyiii <mezofimate@gmail.com>
2022-07-01 18:37:44 +0200

    select created - values got from product table

--- resources/views/production.blade.php
+++ resources/views/production.blade.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Collection;
 
-$rows = DB::table('production')->orderBy('id')->get();
+$productionRows = DB::table('production')->orderBy('id')->get();
+$productRows = DB::table('products')->orderBy('id')->get();
 ?>
 
 <!DOCTYPE html>
@@ -15,32 +17,42 @@ $rows = DB::table('production')->orderBy('id')->get();
 </head>
 
 <body>
-    <div class="productions">
-        <table class="table table-bordered">
-            <thead>
-                <th>Id</th>
-                <th>Pcb_Id</th>
-                <th>Quantity</th>
-                <th>startDate</th>
-                <th>endDate</th>
-                <th>Functions</th>
-            </thead>
-            <tbody>
-                @foreach($rows as $row)
-                <tr>
-                    <td>{{$row->id}}</td>
-                    <td>{{$row->pcb_id}}</td>
-                    <td>{{$row->quantity}}</td>
-                    <td>{{$row->startDate}}</td>
-                    <td>{{$row->endDate}}</td>
-                    <td>
-                        <button type="submit" title="delete">
-                            <i class="fas fa-trash fa-lg text-danger">Delete</i>
-                        </button>
-                    </td>
-                </tr>
+    <container class="container-fluid">
+        <div class="product">
+            <label for="product">Choose a PCB:</label>
+            <select name="product" id="product">
+                @foreach($productRows as $row)
+                <option value="{{ $row->id}}"> {{ $row->pcb}}</option>
                 @endforeach
-            </tbody>
-        </table>
-    </div>
+            </select>
+        </div>
+        <div class="productions">
+            <table class="table table-bordered">
+                <thead>
+                    <th>Id</th>
+                    <th>Pcb_Id</th>
+                    <th>Quantity</th>
+                    <th>startDate</th>
+                    <th>endDate</th>
+                    <th>Functions</th>
+                </thead>
+                <tbody>
+                    @foreach($productionRows as $row)
+                    <tr>
+                        <td>{{$row->id}}</td>
+                        <td>{{$row->pcb_id}}</td>
+                        <td>{{$row->quantity}}</td>
+                        <td>{{$row->startDate}}</td>
+                        <td>{{$row->endDate}}</td>
+                        <td>
+                            <button type="submit" title="delete">
+                                <i class="fas fa-trash fa-lg text-danger">Delete</i>
+                            </button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </container>
 </body>
