e89adc91824245b47e566d9c4ed66d7dc68c576f
matyiii <mezofimate@gmail.com>
2022-07-01 20:29:30 +0200

    Controller created

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
-        $this->middleware('auth');
+        
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
+    public function production()
     {
-        return view('home');
+        $productionRows = DB::table('production')->orderBy('id')->get();
+        $productRows = DB::table('products')->orderBy('id')->get();
+        return view('production',['productionRows'=>$productionRows,'productRows'=>$productRows]);
     }
 }
