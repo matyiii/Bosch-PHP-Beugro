1a5df8acf2e8b3f5b74df693b4198af7af2af21e
matyiii <mezofimate@gmail.com>
2022-07-04 17:08:33 +0200

    center function added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,4 +27,8 @@ class HomeController extends Controller
     public function about(){
         return view('about');
     }
+
+    public function center(){
+        return view('center');
+    }
 }
