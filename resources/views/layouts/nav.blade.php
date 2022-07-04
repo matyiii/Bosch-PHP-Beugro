7c03d1fd35ccc98238d105c433b1a857ca3fb785
matyiii <mezofimate@gmail.com>
2022-07-04 15:54:23 +0200

    about route added

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -6,7 +6,7 @@
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-item nav-link" href="{{ route('production') }}">Production</a>
-      <a class="nav-item nav-link" href="#">About</a>
+      <a class="nav-item nav-link" href="{{ route('about') }}">About</a>
     </div>
   </div>
   <span class="navbar-text">
