d5b0be5b70d9079f06a9584b4555438a4ee9a3e4
matyiii <mezofimate@gmail.com>
2022-07-04 17:07:55 +0200

    center route added

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -6,6 +6,7 @@
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-item nav-link" href="{{ route('production') }}">Production</a>
+      <a class="nav-item nav-link" href="{{ route('center') }}">Center</a>
       <a class="nav-item nav-link" href="{{ route('about') }}">About</a>
     </div>
   </div>
